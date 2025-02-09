ab31b067232e2ccd67b6bf3ad18f1af0fe03b63e
Manoel Lima <manoelsslima@yahoo.com.br>
2025-02-09 18:56:05 -0400

    feat: creating controlled elements

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -20,17 +22,36 @@ function Logo() {
 }
 
 function Form() {
+  const [quantity, setQuantity] = useState(1);
+  const [description, setDescription] = useState("");
+
   // receive an event from form (onSubmit)
   function handleSubmit(e) {
     // avoid the submission
     e.preventDefault();
-    console.log(e);
+
+    // avoid submit withou description
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    initialItems.push(newItem);
+
+    setQuantity(1);
+    setDescription("");
   }
   return (
     // onSubmit={handleSubmit} is the same as onSubmit={(e) => handleSubmit(e)}
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      {/* as it's a number, we need to convert. It can be done adding + before
+      setQuantity(+e.target.value) or Number(e.target.value)
+      */}
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {/* (currentValue, index) */}
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
@@ -38,7 +59,14 @@ function Form() {
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      {/* binding do valor do form com um State. We also need to change the State using
+      the setter method then passes the value of the event (event.target.value). */}
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
