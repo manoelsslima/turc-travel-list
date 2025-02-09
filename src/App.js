773b0494b09beb49160ac1fc64f7884e54c24851
Manoel Lima <manoelsslima@yahoo.com.br>
2025-02-09 10:47:15 -0400

    style: adjust CSS file

---
+++ src/App.js
@@ -0,0 +1,20 @@
+export default function App() {
+  return (
+    <div>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 👜</h1>;
+}
+
+function Form() {}
+
+function PackingList() {}
+
+function Stats() {}
